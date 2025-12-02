882d77726c79e2f8318b74404afee49a146fd7a8
soddoff <soddoff@gmail.com>
2025-12-01 18:41:20 -0800

    Bookbot C2L3

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
+
+def get_book_text(file_path):
+    with open(file_path) as f:
+        return f.read()
+
+
+main()
+
