44f80ee9eb9852b0d11318bf15bedd344c3a9b07
soddoff <soddoff@gmail.com>
2025-12-02 18:31:05 -0800

    Finished boot.dev Bookbot project

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
+import sys
 from stats import *
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
-    num_words = get_num_words(book_path)
-#    print("----------- Word Count ----------")
+    num_words = get_num_words(text)
+    char_dist = count_letters(text)
+    char_list = sort_dictionary(char_dist)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-
+    print("----------- Letter Count ----------")
+    for char_dist in char_list:
+        if char_dist["char"].isalpha():
+            print(f"{char_dist['char']}: {char_dist['num']}")
+    print("============= END ===============")
 
 main()
-
--- stats.py
+++ stats.py
@@ -1,34 +1,35 @@
+#Open and return the file contents 
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
 
 
-def get_num_words(file_path):
-    with open(file_path) as g:
-        words = g.read()
-        return len(words.split())
+#Open and return the number of works in file
+def get_num_words(text):
+    words = text.split()
+    return len(words)
 
-def get_num_characters(file_path)
-    with open(file_path) as h:
-        letters = h.read()
-        dictionary = {}
-        for letter in letters:
-            for char in list(word):
-                if char.lower() not in dictionary:
-                    dictionary[char.lower()] = 1
-                else:
-                    dictionaryf[char.lower()] += 1
-        return dictionary
-
-
-def sort_on(dist):
-    return dist["num"]
+#Convert files characters to lower case. Add character to dictionary and count each instance 
+def count_letters(text):
+    word_list = text.split()
+    dictionary = {}
+    for words in word_list:
+        for char in list(words):
+            if char.lower() not in dictionary:
+                dictionary[char.lower()] = 1
+            else:
+                dictionary[char.lower()] += 1
+    return dictionary
 
 
+def sort_chars(sorted):
+    return sorted["num"]
+
+
+#Sort the dictionary output
 def sort_dictionary(dictionary):
     sorted_list = []
-    for char, count in disctionary.items():
-        sorted_list.append({'char': char})
-#, 'num': count})
-#    sorted_list.sort(reverse=True, key=sort_on)
+    for char, count in dictionary.items():
+        sorted_list.append({'char': char, 'num': count})
+    sorted_list.sort(reverse=True, key=sort_chars)
     return sorted_list
